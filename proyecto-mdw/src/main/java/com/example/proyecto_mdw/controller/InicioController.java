9aed32ca72f0187e70015f3da9e6d1e00f14a31f
Romulo <romulopasache0@gmail.com>
2025-05-12 09:48:02 -0500

    json en inicio para insertar las cartas de los juegos

--- proyecto-mdw/src/main/java/com/example/proyecto_mdw/controller/InicioController.java
+++ proyecto-mdw/src/main/java/com/example/proyecto_mdw/controller/InicioController.java
@@ -1,25 +1,30 @@
 package com.example.proyecto_mdw.controller;
 
+import java.io.InputStream;
+import java.util.Collections;
 import java.util.List;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.proyecto_mdw.model.Inicio;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class InicioController {
     @GetMapping("/")
     public String index(Model model) {
-        // Eliminamos la lista carrusel ya que el carrusel es estático
-        List<Inicio> juegos = List.of(
-            new Inicio("Fortnite", "Juego de batalla campal", "juegos/juego1.avif"),
-            new Inicio("Valorant", "Juego de disparos táctico", "juegos/valorant.webp"),
-            new Inicio("Minecraft", "Juego de construcción y aventuras", "juegos/minecraft.jpg")
-        );
-
-        model.addAttribute("juegos", juegos);
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream is = new ClassPathResource("static/data/juegos.json").getInputStream();
+            List<Inicio> juegos = mapper.readValue(is, new TypeReference<List<Inicio>>() {});
+            model.addAttribute("juegos", juegos);
+        } catch (Exception e) {
+            model.addAttribute("juegos", Collections.emptyList());
+        }
         return "index";
     }
 }
