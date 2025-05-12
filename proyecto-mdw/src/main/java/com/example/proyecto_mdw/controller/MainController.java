b78d6b4753859c58fd415641c16397a55813f168
xSOVxx <diegootega1045@gmail.com>
2025-05-12 08:55:52 -0500

    menu de hambuerguesa funcional denuevo, elimine el mapping de index en main controller

--- proyecto-mdw/src/main/java/com/example/proyecto_mdw/controller/MainController.java
+++ proyecto-mdw/src/main/java/com/example/proyecto_mdw/controller/MainController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index"; // Redirige a index.html
-    }
-
     @GetMapping("/soporte")
     public String soporte() {
         return "soporte"; // Redirige a soporte.html
