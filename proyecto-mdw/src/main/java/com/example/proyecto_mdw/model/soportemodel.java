c31f8334a885ff318b274a9366731f11dd13e29e
Fabian_123 <fabianabadmija@gmail.com>
2025-05-12 11:19:31 -0500

    Add files via upload
    models de soporte

---
+++ proyecto-mdw/src/main/java/com/example/proyecto_mdw/model/soportemodel.java
@@ -0,0 +1,89 @@
+package com.example.proyecto_mdw.model;
+
+import java.util.List;
+
+public class soportemodel {
+
+    private List<MetodoPago> metodos_pago;
+    private String facturacion;
+    private String cambiar_contrasena;
+    private String actualizar_datos;
+    private String cerrar_sesion;
+    private String descargas;
+    private String errores_frecuentes;
+    private String actualizaciones;
+    private String mensaje;
+
+    // Getters y Setters
+    public List<MetodoPago> getMetodos_pago() {
+        return metodos_pago;
+    }
+
+    public void setMetodos_pago(List<MetodoPago> metodos_pago) {
+        this.metodos_pago = metodos_pago;
+    }
+
+    public String getFacturacion() {
+        return facturacion;
+    }
+
+    public void setFacturacion(String facturacion) {
+        this.facturacion = facturacion;
+    }
+
+    public String getCambiar_contrasena() {
+        return cambiar_contrasena;
+    }
+
+    public void setCambiar_contrasena(String cambiar_contrasena) {
+        this.cambiar_contrasena = cambiar_contrasena;
+    }
+
+    public String getActualizar_datos() {
+        return actualizar_datos;
+    }
+
+    public void setActualizar_datos(String actualizar_datos) {
+        this.actualizar_datos = actualizar_datos;
+    }
+
+    public String getCerrar_sesion() {
+        return cerrar_sesion;
+    }
+
+    public void setCerrar_sesion(String cerrar_sesion) {
+        this.cerrar_sesion = cerrar_sesion;
+    }
+
+    public String getDescargas() {
+        return descargas;
+    }
+
+    public void setDescargas(String descargas) {
+        this.descargas = descargas;
+    }
+
+    public String getErrores_frecuentes() {
+        return errores_frecuentes;
+    }
+
+    public void setErrores_frecuentes(String errores_frecuentes) {
+        this.errores_frecuentes = errores_frecuentes;
+    }
+
+    public String getActualizaciones() {
+        return actualizaciones;
+    }
+
+    public void setActualizaciones(String actualizaciones) {
+        this.actualizaciones = actualizaciones;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
